dd4ce85e84ff70a567a2156b39243176672d11ca
Matt Miller <mattmillerart+github@gmail.com>
2020-04-06 16:36:29 -0700

    Initial database work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PouchDB from "pouchdb";
 
 function App() {
+  var db = new PouchDB("http://localhost:5984/kittens");
+
+  db.info().then(function (info) {
+    console.log(info);
+  });
+
+  db.allDocs({ include_docs: true }).then(function (doc) {
+    console.log(">>>", doc);
+  });
+
+  db.changes({
+    since: new Date().getTime(),
+    live: true,
+    include_docs: true
+  })
+    .on("change", function (change) {
+      // received a change
+      console.log('something was changed');
+      console.log(change);
+    })
+    .on("error", function (err) {
+      // handle errors
+      console.error('something was errored');
+      console.error(err);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
