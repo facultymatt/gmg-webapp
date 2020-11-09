fe165d830f31dfc43540ebdc517743dea78167d4
Matt Miller <mattmillerart+github@gmail.com>
2020-11-08 22:28:41 -0800

    Checking in

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { useMemo } from "react";
 import '../node_modules/react-vis/dist/style.css';
 import "./App.css";
 import { GrillStatusContextProvider } from "./contexts/GrillStatusContext";
-import RecentGrillStatusGraph from "./RecentGrillStatusGraph";
+import BarGraph from "./BarGraph";
 
 function App() {
   return useMemo(
     () => (
       <GrillStatusContextProvider>
-        <RecentGrillStatusGraph />
-        {/* <NivoGraphTest /> */}
+        <BarGraph />
       </GrillStatusContextProvider>
     ),
     []
