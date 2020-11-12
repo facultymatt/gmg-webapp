01f869950001c9263fe7c6ee1afdd3c2c0e62e6a
Matt Miller <mattmillerart+github@gmail.com>
2020-11-12 09:36:05 -0800

    Working test

--- src/services/commands/off.js
+++ src/services/commands/off.js
@@ -1,5 +1,14 @@
+import { reject } from "lodash";
+
 export const URL = `http://${window.location.hostname}:3000/command/power/off`;
 
-export default function () {
-  return fetch(URL).then((response) => response.json());
+export default async function () {
+  const confirmResponse = await fetch(URL).then((r) => r.json());
+  const { link } = confirmResponse;
+  return async function (confirm) {
+    if (confirm === true) {
+      return await fetch(link).then((r) => r.json());
+    }
+    return Promise.resolve({ message: "Command disregarded" });
+  };
 }
--- src/services/commands/off.spec.js
+++ src/services/commands/off.spec.js
@@ -7,6 +7,9 @@ import off, { URL as OFF_URL } from "./off";
 describe("off", () => {
   let code;
   beforeEach(() => {
+    
+  });
+  it("works to confirm", async () => {
     code = "123";
     fetch.mockResponseOnce(
       JSON.stringify({
@@ -15,11 +18,26 @@ describe("off", () => {
         code,
       })
     );
+    const confirmFn = await off();
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        message: "Sent command off to grill"
+      })
+    );
+    const result2 = await confirmFn(true);
+    expect(result2.message.includes("Sent command")).toEqual(true);
   });
-  it("works", async () => {
-    const result = await off();
-    expect(result.message.includes("Confirm")).toEqual(true);
-    expect(result.link.includes(OFF_URL)).toEqual(true);
-    expect(result.code).toEqual(code);
+  it("works to NOT confirm", async () => {
+    code = "123";
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        message: "Confirm you want send command off by visiting this link now",
+        link: `${OFF_URL}?code=${code}`,
+        code,
+      })
+    );
+    const confirmFn = await off();
+    const result2 = await confirmFn(false);
+    expect(result2.message.includes("disregarded")).toEqual(true);
   });
 });
